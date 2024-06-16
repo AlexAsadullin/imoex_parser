b8f6eb6ec293308a13e5030890dde8d867691b54
Alex Asadullin <151203706+AlexAsadullin@users.noreply.github.com>
2024-06-16 18:08:04 +0300

    get_stock_history -> get_stock_ prices. get_stock_histor now is new func with no params just to call get_stock_ prices func from main body. Now we can load any amount of data for any dates (without 4 months barrier

--- imoex_parser.py
+++ imoex_parser.py
@@ -4,14 +4,43 @@ import json
 from datetime import datetime
 from datetime import date, timedelta
 
+DATE_FORMAT = '%Y-%m-%d'
+
+
 def save_data(name: str, data, extension: str):
-    with open(f'{name}.{extension}', 'w') as file:
+    with open(f'data/{name}.{extension}', 'w') as file:
         if extension == 'json':
             file.write(json.dumps(data))
         elif extension == 'csv' and 'DataFrame' in str(type(data)):
-            file.write(data.to_scv())
+            file.write(data.to_csv())
         else:
             print('unable to save your data type yet, please, try to save it by yourself')
+
+
+def dates_range(year_begin, month_begin, day_begin, year_end=None, month_end=None, day_end=None, endnow=True,
+                noweekends=True):
+    begin = date(year_begin, month_begin, day_begin)
+
+    if endnow:
+        ending = date.today() - timedelta(days=1)
+    else:
+        ending = date(year_end, month_end, day_end) - timedelta(days=1)
+
+    delta = timedelta(days=1)
+    dates = []
+
+    if not noweekends:
+        while begin <= ending:
+            dates.append(begin.strftime(DATE_FORMAT))
+            begin += delta
+    else:
+        while begin <= ending:
+            weekday = begin.weekday()
+            if weekday != 5 and weekday != 6:
+                dates.append(begin.strftime(DATE_FORMAT))
+            begin += delta
+
+    return dates
 
 
 def get_general_info(file_type='json', encoding='utf-8'):
@@ -25,49 +54,36 @@ def get_general_info(file_type='json', encoding='utf-8'):
     return all_stocks_df
 
 
-def get_stock_history(begin_date, end_date, ticker: str, encoding='utf-8'):
+def get_stock_prices(begin_date, end_date, ticker: str, encoding='utf-8'):
     engines = {'stock': 'stock', }
     markets = {'shares': 'shares', 'bonds': 'bonds', 'foreignshares': 'foreignshares'}
     url = f"https://iss.moex.com/iss/history/engines/{engines['stock']}/markets/{markets['shares']}/securities/{ticker}.json?from={begin_date}&till={end_date}&marketprice_board=1"
-    #url = 'https://iss.moex.com/iss/history/engines/stock/markets/shares/securities/MOEX.json?from=2023-01-01&till=2023-05-31&marketprice_board=1'
+    # url = 'https://iss.moex.com/iss/history/engines/stock/markets/shares/securities/MOEX.json?from=2023-01-01&till=2023-05-31&marketprice_board=1'
     print(url)
     request = requests.get(url)
-    save_data(f'{ticker}_{begin_date}_{end_date}', request.text, extension='json')
     data = json.loads(request.text)
     df = pd.DataFrame(data['history']['data'], columns=data['history']['columns'])
     return df
 
 
-def get_more_4months(begin_date: datetime, end_date: datetime, ticker):
-    sec_to_mon = 0
-    difference = end_date.timestamp() - begin_date.timestamp()
-    sec_to_day = 1 / (60 * 60 * 24 * 30)
-    for i in range(int(difference * sec_to_day)):
+def get_stock_history(MAX_LOAD_ONCE=100):
+    dates = dates_range(2020, 5, 30, endnow=True, noweekends=True)
+    ticker = 'MOEX'
+    stock_df = pd.DataFrame()
+    for i in range(0, len(dates) // MAX_LOAD_ONCE + 1):
+        if i * MAX_LOAD_ONCE + MAX_LOAD_ONCE < len(dates):
+            df = get_stock_prices(ticker=ticker,
+                                  begin_date=dates[i * MAX_LOAD_ONCE],
+                                  end_date=dates[i * MAX_LOAD_ONCE + MAX_LOAD_ONCE])
+            stock_df = pd.concat([stock_df, df], ignore_index=True)
+        else:
+            df = get_stock_prices(ticker=ticker,
+                                  begin_date=dates[i * MAX_LOAD_ONCE],
+                                  end_date=dates[len(dates) - 1])
+            stock_df = pd.concat([stock_df, df], ignore_index=True)
 
-
-
-    imoex_history = get_stock_history(begin_date=begin, end_date=actual_time, ticker='MOEX')
-
-
-def dates_range(year_begin, month_begin, day_begin, year_end, month_end, day_end, endnow: bool):
-    begin = date(year_begin, month_begin, day_begin)
-    ending = date.today() - timedelta(days=1)
-    delta = timedelta(days=1)
-    dates = []
-    while begin <= ending:
-        dates.append(begin.strftime('%Y-%m-%d'))
-        begin += delta
-    return dates
-
+    save_data(f'{ticker}_{dates[0]}_{dates[-1]}', stock_df, 'csv')
 
 
 if __name__ == '__main__':
-    actual_time = datetime.now()
-    print(actual_time)
-    begin = datetime.strptime('2020-05-30', '%Y-%m-%d')
-    history = get_stock_history(begin_date=begin, end_date=actual_time, ticker='MOEX')
-
-
-
-    print(len(imoex_history))
-    imoex_history.to_csv(f'imoex_{begin}.csv')
+    get_stock_history()
